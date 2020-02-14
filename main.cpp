7d316a9f0fe489ee14bf8e581a234206274fb32a
Nino Kettlitz <1396039+ninok@users.noreply.github.com>
2020-02-14 19:55:52 +0100

    Added hyrise/sql-parser as submodule

--- main.cpp
+++ main.cpp
@@ -1,25 +1,45 @@
 #include <iostream>
 #include <boost/algorithm/string.hpp>
 
+#include <SQLParser.h>
+#include <SQLParserResult.h>
+#include <sql/CreateStatement.h>
+
 int main(int argc, char **argv)
 {
     std::cout << "Please enter sql command. exit to leave the programm" << std::endl;
 
-    std::string command;
+    std::string sqlCommand;
+    hsql::SQLParserResult sqlParserResult;
 
     while (true)
     {
-        std::cin >> command;
-        boost::to_lower(command);
-        std::stringstream commandstr(command);
+        std::getline(std::cin, sqlCommand);
+        // boost::to_lower(sqlCommand);
 
         std::string operation;
-        if (command == "exit")
+        if (sqlCommand == "exit")
         {
             break;
         }
-        else if (command == "create")
+
+        const auto valid = hsql::SQLParser::parse(sqlCommand, &sqlParserResult);
+        if (!valid || !sqlParserResult.isValid())
         {
+            std::cout << sqlCommand << std::endl;
+            std::cout << sqlParserResult.errorMsg() << std::endl;
+            continue;
+        }
+
+        const auto &statements = sqlParserResult.getStatements();
+
+        for (const auto *statement : statements)
+        {
+            if (statement->isType(hsql::kStmtCreate))
+            {
+                const hsql::CreateStatement *create = static_cast<const hsql::CreateStatement *>(statement);
+                std::cout << create->tableName << std::endl;
+            }
         }
     }
 
